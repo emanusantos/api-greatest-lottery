925b11e2f6e998d6b7586b3265088403fab2f587
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-08-16 15:53:15 -0300

    added mailtrap confirmation to new user

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,14 +1,26 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class UserController {
     async store ({ request }) {
-        const data = request.only(['name', 'email', 'password'])
+        try {
+            const data = request.only(['name', 'email', 'password'])
 
-        const user = await User.create(data)
+            const user = await User.create(data)
 
-        return user
+            await Mail.send(['emails.new_user'], { name: user.name }, message => {
+                message
+                .to(user.email)
+                .from('emanuel.santos@luby.software', 'Emanuel | Luby Software')
+                .subject('Boas vindas ao TGL')
+            })
+
+            return user
+        } catch (err) {
+            return response.status(err.status).send({ error: { message: "Algo não deu certo com o seu registro" } })
+        }
     }
 }
 
